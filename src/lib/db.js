28c53dcc62910b69226d816dcf54334a8ff1682b
WilliamSnjy <william.sanjaya50@gmail.com>
2026-07-17 17:37:27 +0700

    update db flex

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,10 @@
-import { Pool } from 'pg';
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_LOCAL_URL
+    connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
